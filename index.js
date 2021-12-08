8b24b0c7c0017b9a7094cfb890d6aeb8c2e4ae9a
Hal <hgrforrest@gmail.com>
2021-12-08 14:46:42 -0500

    chaining promises tests :comet: :ring:

--- index.js
+++ index.js
@@ -1,41 +1,5 @@
 const fs = require('fs')
 
-fs.readFile('./file.json', (err, data) => {
-	if (err) {
-		// error handling
-		console.log(err);
-		return
-	}
-
-	// handle data normally if no errors
-	console.log(JSON.parse(data).favGames);
-})
-
-let done = false
-
-const isComplete = new Promise((resolve, reject) => {
-	if (done) {
-		const workDone = "The work has completed"
-		resolve(workDone)
-	}
-	else {
-		const why = "The work has not yet completed"
-		reject(why)
-	}
-})
-
-const checkCompleteness = () => {
-	isComplete
-		.then(ok => {
-			console.log(ok);
-		})
-		.catch(err => {
-			console.error(err);
-		})
-}
-
-checkCompleteness()
-
 const getFile = (filename) => {
 	return new Promise((resolve, reject) => {
 		fs.readFile(filename, (err, data) => {
@@ -45,13 +9,39 @@ const getFile = (filename) => {
 				reject(err)
 				return
 			}
-		
+
 			// handle data normally if no errors
 			resolve(data)
 		})
-	})	
+	})
 }
 
+const status = response => {
+	if (response) {
+		console.log('We got a non null response', response);
+		return Promise.resolve(response)
+	}
+	console.log('We did not get a non null response');
+	return Promise.reject(new Error(response))
+}
+
+const json = ((data) => {
+	try {
+		const jsonData = JSON.parse(data)
+		console.log('We parsed the data', jsonData);
+		return jsonData
+	} catch (err) {
+		console.log('The data couldnt be parsed');
+		return false
+	}
+})
+
+const loggit = ((responseData) => {
+	console.log('Here is the data from the file: ', responseData)
+})
+
 getFile('./file.json')
-	.then(data => console.log(JSON.parse(data)))
+	.then(status)
+	.then(json)
+	.then(loggit)
 	.catch(err => console.error(err))
